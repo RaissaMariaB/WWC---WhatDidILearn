4e76000d2ab91dccac87b173ffb57cc3e4618d0a
RaissaBarata <raissapereira@sas.com.br>
2020-10-31 12:07:45 -0300

    wip - create linear layout

--- app/src/main/java/com/wwc/whatdidilearn/views/MainActivity.kt
+++ app/src/main/java/com/wwc/whatdidilearn/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.wwc.whatdidilearn
+package com.wwc.whatdidilearn.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wwc.whatdidilearn.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
